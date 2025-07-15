a33ac8b91e1717a628d5b1210c2cdd16f97f1e97
Jun Aishima <jaishima@bnl.gov>
2025-07-15 12:04:11 -0400

    pre-commit fixes

--- run-deployment.py
+++ run-deployment.py
@@ -5,6 +5,6 @@ from prefect.deployments import run_deployment
 run_deployment(
     name="end-of-run-workflow/test-workflow-prefect3-sandbox-deploy",
     # name="end-of-run-workflow/end_of_run_workflow_deployment",
-    parameters = {"stop_doc": {"run_start": "47c21f4d"}}
+    parameters={"stop_doc": {"run_start": "47c21f4d"}},
     timeout=15,  # don't wait for the run to finish # edit to 15 sec
 )
