b265767d2322465a68e13351911331b2822f5fef
Jun Aishima <jaishima@bnl.gov>
2025-07-15 11:28:18 -0400

    remove bad python command call

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -18,8 +18,6 @@ def log_completion():
 @flow(log_prints=True)
 def end_of_run_workflow(stop_doc):
     logger = get_run_logger()
-    import subprocess
-    a=subprocess.run(["echo testing"])
     import os
     print(f"TILED_API_KEY: {os.environ['TILED_API_KEY'][:3]}")
     a=subprocess.run(["ls","/nsls2/software/etc/tiled/profiles"])
